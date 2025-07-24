e7e9105869845e99c162f297fade2142780fea81
Niyati-Saxena <niyatisaxena31@gmail.com>
2025-07-24 10:39:27 +0530

    Create Mini_project_4.py

---
+++ Data_Structures/Mini_project_4.py
@@ -0,0 +1,5 @@
+# Given a string of n words, help Alex to find out how many times his name appears in the string.
+
+text = input("Enter the string: ")
+count = text.count("Alex")
+print("Number of times 'Alex' appears:", count)
